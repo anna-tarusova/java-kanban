52ad448db42da3a6aed5708ebec901601131e1ab
Anna Tarusova <Anna_Tarusova@epam.com>
2024-10-18 16:01:55 +0300

    Удалён лишний код

--- src/ru/yandex/practicum/tasks/model/Epic.java
+++ src/ru/yandex/practicum/tasks/model/Epic.java
@@ -3,36 +3,10 @@ package ru.yandex.practicum.tasks.model;
 import ru.yandex.practicum.tasks.model.enums.TaskType;
 
 public class Epic extends BaseTask {
-//    private List<Integer> subtasksIds = new ArrayList<>();
 
     public Epic(String name, String description) {
         super(name, description);
     }
-
-//    public List<Integer> getSubtasksIds() {
-//        return new ArrayList<>(subtasksIds);
-//    }
-
-//    public void addSubtask(Subtask subtask) {
-//        int subtaskId = subtask.getId();
-//
-//        if (!subtasksIds.contains(subtaskId)) {
-//            subtasksIds.add(subtaskId);
-//        }
-//    }
-
-//    public void removeSubtaskById(int subtaskId) {
-//        int index = subtasksIds.indexOf(subtaskId);
-//        if (index == -1) {
-//            return;
-//        }
-//        subtasksIds.remove(index);
-//    }
-
-//    public void clearAllSubtaskIds() {
-//        subtasksIds = new ArrayList<>();
-//    }
-
 
     @Override
     public TaskType getTaskType() {
